156b442b2b14f945334e7b6f72b7cc55b0c2d56b
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-12-22 16:01:42 -0500

    18 part 2 wip

---
+++ dmmulroy/18/actor.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const sanitizedInput = input.split('\n').map(row => row.split(' '));
+
+class Actor {
+  constructor(id, instructions) {
+    this.id = id;
+    this.instructions = instructions;
+    this.mailbox = [];
+
+    this.enqueueMailbox = this.enqueueMailbox.bind(this);
+    process.on('message', msg => {
+      console.log('here!!');
+      if (msg['value']) {
+        this.enqueueMailbox(msg['value']);
+      } else if (msg['start']) {
+        this.processInstructions();
+      }
+    });
+  }
+
+  setSendCallback(cb) {
+    this.sendCallback = cb;
+  }
+
+  enqueueMailbox(item) {
+    this.mailbox.push(item);
+  }
+
+  dequeueMailbox() {
+    return new Promise((resolve, reject) => {
+      setInterval(() => {
+        if (this.mailbox.length > 0) resolve(this.mailbox.shift());
+      }, 100);
+    });
+  }
+
+  async processInstructions() {
+    try {
+      let sendCount = 0;
+      let { instructions } = this;
+      let register = { p: this.id };
+      let recoveredFrequency = 0;
+
+      for (let idx = 0; idx < instructions.length; idx++) {
+        const [op, registerKey, value] = instructions[idx];
+
+        if (!register[registerKey]) register[registerKey] = 0;
+
+        switch (op) {
+          case 'set':
+            register[registerKey] = register[value]
+              ? register[value]
+              : Number(value);
+            break;
+          case 'add':
+            register[registerKey] = register[value]
+              ? register[value] + register[registerKey]
+              : Number(value) + register[registerKey];
+            break;
+          case 'mul':
+            register[registerKey] = register[value]
+              ? register[value] * register[registerKey]
+              : Number(value) * register[registerKey];
+            break;
+          case 'mod':
+            register[registerKey] = register[value]
+              ? register[registerKey] % register[value]
+              : register[registerKey] % Number(value);
+            break;
+          case 'snd':
+            const sendValue = register[registerKey]
+              ? register[registerKey]
+              : Number(value);
+
+            console.log('Actor', this.id, 'sending', sendValue);
+
+            process.send({ id: this.id, value: sendValue });
+            sendCount++;
+            break;
+          case 'jgz':
+            if (register[registerKey] > 0) idx += Number(value) - 1;
+            break;
+          case 'rcv':
+            register[registerKey] = await this.dequeueMailbox();
+            break;
+          default:
+            throw new Error('Something went wrong');
+            break;
+        }
+      }
+      process.send({ id: this.id, sendCount });
+    } catch (err) {
+      console.log('Actor', this.id, 'Error:', err);
+    }
+  }
+}
+
+new Actor(process.argv[2], process.argv[3]);
---
+++ dmmulroy/18/part2.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { fork } = require('child_process');
+
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const sanitizedInput = input.split('\n').map(row => row.split(' '));
+
+const Actor1 = fork('actor.js', [0, sanitizedInput]);
+const Actor2 = fork('actor.js', [1, sanitizedInput]);
+
+Actor1.on('message', msg => {
+  console.log('actor1 msg', msg);
+  if (msg['sendCount']) {
+    console.log('output:', msg['sendCount']);
+    process.exit();
+  } else {
+    Actor2.send(msg);
+  }
+});
+
+Actor2.on('message', msg => {
+  console.log('actor2 msg', msg);
+  Actor2.send(msg);
+});
+
+Actor1.send({ start: true });
+Actor2.send({ start: true });
